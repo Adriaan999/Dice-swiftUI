439832e8d76129a04c14abcc67526a2188d5e9a6
Adriaan999 <Adriaan9@hotmail.co.za>
2021-03-29 09:11:43 +0200

    init

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Adriaan van Schalkwyk on 2021/02/10.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
